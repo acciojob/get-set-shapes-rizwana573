3f7ebd3c2decda9a112a6b1e5e09fb6ac682e4e4
rizwana573 <95849132+rizwana573@users.noreply.github.com>
2023-10-19 21:51:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width, height){
+		this.width=width;
+		this.height=height;
+	}
+	getWidth(){
+		return this.width;
+	}
+	setWidth(newWidth){
+		this.width= newWidth;
+	}
+	getHeight(){
+		return this.height;
+	}
+	setWidth(newHeight){
+		this.height= newHeight;
+	}
+	getArea(){
+		return this.width*this.height;
+	}
+	
+}
 
-class Square extends Animal {}
+class Square extends Rectangle {
+	getPerimeter(){
+		console.log(4*this.width);
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
